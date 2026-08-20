b81163cd54f64a0e8afc9435331f41bb5b2cbfb7
DevDon-iOS <dennis2327@naver.com>
2026-08-20 15:47:30 +0900

    [feat] Apple Watch 핵심 흐름 구현

--- sip Watch App/sipApp.swift
+++ sip Watch App/sipApp.swift
@@ -11,7 +11,36 @@ import SwiftUI
 struct sip_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            rootView
         }
     }
+
+    @ViewBuilder
+    private var rootView: some View {
+#if DEBUG
+        if let previewState = ProcessInfo.processInfo.arguments.watchPreviewState {
+            if ProcessInfo.processInfo.arguments.contains("-SIPWatchAccessibilityText") {
+                ContentView(previewState: previewState)
+                    .environment(\.dynamicTypeSize, .accessibility5)
+            } else {
+                ContentView(previewState: previewState)
+            }
+        } else {
+            ContentView()
+        }
+#else
+        ContentView()
+#endif
+    }
 }
+
+#if DEBUG
+private extension Array where Element == String {
+    var watchPreviewState: WatchPreviewState? {
+        guard let flagIndex = firstIndex(of: "-SIPWatchPreviewState") else { return nil }
+        let valueIndex = index(after: flagIndex)
+        guard indices.contains(valueIndex) else { return nil }
+        return WatchPreviewState(rawValue: self[valueIndex])
+    }
+}
+#endif
